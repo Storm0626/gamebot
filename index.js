55f39bad35173e8fbebb4266fce20bead54ae930
ch33s322 <61012984+ch33s322@users.noreply.github.com>
2020-10-19 15:56:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
-//dakotas shity discord bot lmao
-
+//the game bot
+//version 0.1
 const Discord = require("discord.js"); //library for interacting with discord
 const fs = require("fs"); //library for file system in node js
-
-const TOKEN = ""; //bot token 
-
+const TOKEN = "NzY3ODI0OTQ3MTc5NDIxNzM5.X43itA.OsXLusO5KndKJi0elKpHmM83r24"; //bot token 
 const PREFIX = "!"; //list of prefixes that get the bot's attention
 const PREFIX2 = "!<>";
 const TEXTFIX = "3==>" //lmao
-
-var servers = {};
-
 var Bot = new Discord.Client();
-
-
-//all fortunes for the 8ball command
-var fortunes = [
-    "yes",
-    "maybe",
-    "possibly",
-    "with out a doubt",
-    "it is certain",
-    "ask again",
-    "no",
-    "yee",
-    "naw",
-    "GeT oUt Of My RoOm I'm PlAyInG mInEcRaFt!!!!111!!!!"
-]
 
 Bot.on('ready', () => {
     console.log("ready");
@@ -35,27 +15,25 @@ Bot.on('ready', () => {
 //main commands
 Bot.on("message", function(message) {
     if (message.author.equals(Bot.user)) return; //dont let bots msg bots
-
     if (!message.content.startsWith(PREFIX)) return; //ignore if not start with prefix
-
     var args = message.content.substring(PREFIX.length).split(" ");
 
     switch (args[0].toLocaleLowerCase()) {
         case "ping":
-            message.channel.sendMessage("Pc crashed. Going to try to go to bed again");
+            message.channel.sendMessage("pong");
             break;
-        case "embed":
-            var MEME = new Discord.RichEmbed()
+        case "place":
+            var place = new Discord.RichEmbed()
                 .setColor(0x0fa567)
-                .setTitle('More cowbower content here')
-                .setURL('https://www.reddit.com/r/CowBowser/')
-                .setAuthor('~OwO~', 'https://i.imgur.com/v4Gz4b9.png', 'https://www.reddit.com/r/CowBowser/')
-                .setDescription('The meme that you have called upon:')
+                .setTitle('place')
+                .setURL('')
+                .setAuthor('rpgbot', 'https://i.imgur.com/v4Gz4b9.png', 'https://www.reddit.com/r/CowBowser/')
+                .setDescription('The place that was chosen')
                 .setThumbnail('https://i.imgur.com/RJu2v72.png')
-                .setImage((meme_links[Math.floor(Math.random() * meme_links.length)]))
+                .setImage("https://i.imgur.com/RJu2v72.png")
                 .setTimestamp()
-                .setFooter('I take my leave', 'https://i.imgur.com/v4Gz4b9.png')
-            message.channel.sendEmbed(MEME);
+                .setFooter('bottom text', 'https://i.imgur.com/v4Gz4b9.png')
+            message.channel.sendEmbed(place);
             break;
         case "profile":
             //either get specific user or assign a random profile
@@ -70,7 +48,7 @@ Bot.on("message", function(message) {
                     break;
                 default:
                     var prof = new Discord.RichEmbed()
-                        .addField(message.author.username.toString(), (descriptors[Math.floor(Math.random() * descriptors.length)]))
+                        .addField("text here")
                         .setColor(0x0fa655)
                         .setThumbnail(message.author.avatarURL)
                     message.channel.sendEmbed(prof);
@@ -82,9 +60,7 @@ Bot.on("message", function(message) {
             break;
         default:
             message.channel.sendMessage("Invalid comand");
-
             break;
-
     }
 });
 
@@ -92,13 +68,11 @@ Bot.on("message", function(message) {
 Bot.on("message", function(message) {
     if (message.author.equals(Bot.user)) return;
     if (!message.content.startsWith(PREFIX2)) return;
-
     //actually writes the file
     var words = message.content.substring(PREFIX2.length).split(" ")
     var args = message.content.substring(PREFIX2.length);
     switch (args[0].toLocaleLowerCase()) {
         default: fs.writeFile(words[0].toLocaleLowerCase() + '.txt', args, (err) => {
-
             // In case of a error throw err. 
             if (err) throw err;
         })
@@ -111,13 +85,10 @@ Bot.on("message", function(message) {
     if (message.author.equals(Bot.user)) return;
     if (!message.content.startsWith(TEXTFIX)) return;
     var args = message.content.substring(TEXTFIX.length);
-
     fs.readFile(args.toLocaleLowerCase() + ".txt", "utf8", function(err, data) { //read the file
         if (err) throw err;
         message.channel.sendMessage(data);
-
     });
-
 });
 
 
